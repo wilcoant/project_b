a237c0a1e14dbdd3d6fc4eaa51539fbd7932f275
Anthony Wilcox <wilcoant@onid.oregonstate.edu>
2015-07-28 13:57:35 -0700

    form input complete working on php
    still need to add dbaseconnection and js

---
+++ waterinput.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>FMCSA API</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="blog.css" rel="stylesheet">
+
+   
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="blog-masthead">
+      <div class="container">
+        <nav class="blog-nav">
+          <a class="blog-nav-item active" href="waterinput.php">Home</a>
+          <!-- additional page links for nav bar go here-->
+        </nav>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <div class="blog-header">
+       
+      </div>
+
+      <div class="row">
+
+        <div class="col-sm-8 blog-main">
+
+          <div class="blog-post">
+            <h1 class="blog-title">Smart Water Management</h1>
+            <p class="lead blog-description">Manual and Automated Entry for Soil Saturation Levels</p>
+            <h2 class="blog-post-title">Soil Saturation Input</h2>
+            <fieldset>
+                <form>
+                   <table>
+                    <tr>
+                        <td align = "left">Sensor ID </td>
+                        <td align = "left"><input type = "text" placeholder = "ID Number"></input></td>
+                   </tr>
+                    <tr>
+                        <td align = "left">Soil Saturation Level </td>
+                        <td align = "left"><input type = "number" placeholder = "Saturation Percentage"></input></td>
+                    </tr>
+                    <tr>
+                        <td align = "left">Water Depth</td>
+                        <td align = "left"><input type = "text" placeholder = "Depth of initial saturation detection"></input></td>
+                    </tr>
+                    <tr>
+                        <td align = "left">Date</td>
+                    
+                        <td align = "left"><select>
+                            <option value = "1">1</option>
+                            <option value = "2">2</option>
+                            <option value = "3">3</option>
+                            <option value = "4">4</option>
+                            <option value = "5">5</option>
+                            <option value = "6">6</option>
+                            <option value = "7">7</option>
+                            <option value = "8">8</option>
+                            <option value = "9">9</option>
+                            <option value = "10">10</option>
+                            <option value = "11">11</option>
+                            <option value = "12">12</option>
+                            <option value = "13">13</option>
+                            <option value = "14">14</option>
+                            <option value = "15">15</option>
+                            <option value = "16">16</option>
+                            <option value = "17">17</option>
+                            <option value = "18">18</option>
+                            <option value = "19">19</option>
+                            <option value = "20">20</option>
+                            <option value = "21">21</option>
+                            <option value = "22">22</option>
+                            <option value = "23">23</option>
+                            <option value = "24">24</option>
+                            <option value = "25">25</option>
+                            <option value = "26">26</option>
+                            <option value = "27">27</option>
+                            <option value = "28">28</option>
+                            <option value = "29">29</option>
+                            <option value = "30">30</option>
+                            <option value = "31">31</option>
+                       </td></select>
+                    </tr>
+                    <tr>
+                        <td align = "left">Month</td>
+                        <td align = "left"><select>
+                            <option value = "January">January</option>
+                            <option value = "February">February</option>
+                            <option value = "March">March</option>
+                            <option value = "April">April</option>
+                            <option value = "May">May</option>
+                            <option value = "January">June</option>
+                            <option value = "January">July</option>
+                            <option value = "January">August</option>
+                            <option value = "January">September</option>
+                            <option value = "January">October</option>
+                            <option value = "January">November</option>
+                            <option value = "January">December</option>
+                       </td></select>
+                    </tr>
+                    <tr>
+                        <td align = "left">Year</td>
+                        <td align = "left"><select>
+                            <option value = "2010">2010</option>
+                            <option value = "2011">2011</option>
+                            <option value = "2012">2012</option>
+                            <option value = "2013">2013</option>
+                            <option value = "2014">2014</option>
+                            <option value = "2015">2015</option>
+                       </td></select>
+                    </tr>
+                     <tr>
+                        <td align = "left">Time</td>
+                        <td align = "left">
+                          <select>
+                            <option value = "12">12</option>
+                            <option value = "1">1</option>
+                            <option value = "2">2</option>
+                            <option value = "3">3</option>
+                            <option value = "4">4</option>
+                            <option value = "5">5</option>
+                            <option value = "6">6</option>
+                            <option value = "7">7</option>
+                            <option value = "8">8</option>
+                            <option value = "9">9</option>
+                            <option value = "10">10</option>
+                            <option value = "11">11</option>
+                           
+                         </select>
+                         <select>
+                            <option value = "0">:00</option>
+                            <option value = "5">:05</option>
+                            <option value = "10">:10</option>
+                            <option value = "15">:15</option>
+                            <option value = "20">:20</option>
+                            <option value = "25">:25</option>
+                            <option value = "30">:30</option>
+                            <option value = "35">:35</option>
+                            <option value = "40">:40</option>
+                            <option value = "45">:45</option>
+                            <option value = "50">:50</option>
+                            <option value = "55">:55</option>
+                            
+                         </select>
+
+                         <select>
+                            <option value = "1">AM</option>
+                            <option value = "2">PM</option>
+                        </select>
+                      
+                    </tr>
+                   </table>
+                </form>
+            </fieldset>
+
+           
+            
+           
+           
+          </div><!-- /.blog-post -->
+
+          <!-- we can use this if we want to link a " next " button at the bottom of the page<nav>
+            <ul class="pager">
+              <li><a href="gettingstarted.php">Next</a></li>
+            </ul>
+          </nav>
+        -->
+        </div><!-- /.blog-main -->
+
+        <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
+          
+          <div class="sidebar-module">
+            <h4>Shortcuts</h4>
+            <ol class="list-unstyled">
+              <li><a href="waterinput.php">Home</a></li>
+              
+            </ol>
+          </div>
+          
+          
+        </div><!-- /.blog-sidebar -->
+
+      </div><!-- /.row -->
+
+    </div><!-- /.container -->
+
+    <footer class="blog-footer">
+      <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+      <p>
+        <a href="#">Back to top</a>
+      </p>
+    </footer>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
